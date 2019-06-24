0abe8e68f7eee4b619388ab632344f7412010ca6
Scott Miller <scott@millergeek.xyz>
2019-06-24 13:56:13 -0400

    make types explicit

--- variables.tf
+++ variables.tf
@@ -1,14 +1,17 @@
 variable "domain" {
   description = "Domain to use for SES"
+  type        = string
 }
 
 variable "zone_id" {
   description = "Route 53 zone ID for the SES domain verification"
+  type        = string
   default     = ""
 }
 
 variable "ses_records" {
   description = "Additional entries which are added to the _amazonses record"
+  type        = list(string)
   default     = []
 }
 
